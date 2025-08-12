b704e669ce9ea63290154602132d31d4bb4db99f
Dikko Toheeb <dikkotoheeb@gmail.com>
2025-08-12 15:00:14 +0100

    Update depositRoutes.js

--- backend/routes/depositRoutes.js
+++ backend/routes/depositRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const depositController = require('../controllers/depositController');
 const multer = require('multer');
-
+const uploadProof = require('../middleware/uploadProof');
 // Use memory storage so we can upload to cloud instead of Render's disk
 const storage = multer.memoryStorage();
 const upload = multer({ storage });

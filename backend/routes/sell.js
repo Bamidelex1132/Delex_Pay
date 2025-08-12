6476f8073db17c8057274e4c2f828f1584b074b4
Dikko Toheeb <dikkotoheeb@gmail.com>
2025-08-12 09:49:16 +0100

    Update sell.js

--- backend/routes/sell.js
+++ backend/routes/sell.js
@@ -4,14 +4,7 @@ const router = express.Router();
 
 const uploadProof = require('../middleware/uploadProof'); // Multer + Cloudinary middleware
 const sellController = require('../controllers/sellController');
-const  authMiddleware = require('../middleware/authMiddleware');
-
-// Sell route
-router.post(
-  '/sell',
-  authMiddleware,
-  uploadProof.single('proof'),
-  sellController.submitSell
-);
+const { authenticateUser } = require('../middleware/authMiddleware');
+router.post('/sell', authenticateUser, uploadProof.single('proof'), sellController.submitSell);
 
 module.exports = router;
